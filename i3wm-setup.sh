f7d139f4a1dde19ec3382168bfbf6efeb686c522
Brenno Costa <brennolncosta@gmail.com>
2015-12-28 10:56:52 -0300

    Control i3 with python bindings

---
+++ i3wm-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo easy_install pip
+
+# i3wm bindings for python
+pip install i3ipc
+
+# Control MPD
+sudo pacman -S --noconfirm mpc
+
+# Control pulseaudio devices
+sudo pacman -S --noconfirm pavucontrol
+
+# CLI tool to configure multiple diplays outputs
+sudo pacman -S --noconfirm xorg-xrandr
+
+# CLI Tool to control display brightness
+sudo pacman -S --noconfirm xorg-xbacklight
+
+# Powerful laucher and window switcher (better than dmenu)
+sudo pacman -S --noconfirm rofi
+
+# Tool to set global xconfigs (for xterm for example)
+sudo pacman -S --noconfirm xorg-xrdb
